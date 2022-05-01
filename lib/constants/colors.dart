349a293b292c6d7fe954f82a30952aab81033d75
convrtxJes13 <jestoni@convrtx.com>
2022-05-01 20:31:17 +0800

    changes

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,7 +2,6 @@ library app_colors;
 
 import 'package:flutter/material.dart';
 
-
 Color primaryBlack = const Color(0xff18243C);
 Color primaryYellow = const Color(0xffF9DB97);
 Color facebookColor = const Color(0xff3B5998);
@@ -11,13 +10,5 @@ Color secondaryWhite = const Color(0xffF9F9F9);
 Color primaryRed = const Color(0xffE4572E);
 Color secondaryGray = const Color(0xff6B6968);
 Color primaryOrange = const Color(0xffE75B31);
-
-const Color primaryBlack = Color(0xff18243C);
-const Color primaryYellow = Color(0xffF9DB97);
-const Color facebookColor = Color(0xff3B5998);
-const Color googleColor = Color(0xffEB4132);
-const Color secondaryWhite = Color(0xffF9F9F9);
-const Color primaryRed = Color(0xffE4572E);
-const Color secondaryGray = Color(0xff6B6968);
 const Color scaffoldColor = Color(0xFF1d140c);
 const Color ticketBadgePendingColor = Color(0xFFffa800);
