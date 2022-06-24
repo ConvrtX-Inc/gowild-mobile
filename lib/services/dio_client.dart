447b82d483b33250ff4137391ac8432d8f16be51
mae <ednamae@convrtx.com>
2022-06-24 16:44:10 +0800

    updates

--- lib/services/dio_client.dart
+++ lib/services/dio_client.dart
@@ -116,7 +116,7 @@ class DioClient {
     final String? token =
         await SecureStorage.readValue(key: SecureStorage.userTokenKey);
 
-    const String postEndPoint = baseUrl + '/api/v1/route';
+    const String postEndPoint = baseUrl + '/api/v1/route?sort=created_date,DESC';
     BaseOptions options = BaseOptions(
       baseUrl: postEndPoint,
       connectTimeout: 10000,
